2a0392726a8549eeb533049337a659cea5cc2e6c
Uriolivei <1554602@senati.pe>
2025-06-06 20:49:00 -0500

    Ranita

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,22 @@
-rootProject.name = "CloudstreamPlugins"
+// settings.gradle.kts (archivo de la raíz de tu proyecto TestPlugins)
 
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+// ESTE BLOQUE ES ABSOLUTAMENTE INDISPENSABLE para que Gradle sepa dónde buscar plugins.
+pluginManagement {
+    repositories {
+        // Repositorio oficial de plugins de Gradle.
+        gradlePluginPortal()
+        // Repositorio de Maven Central, donde se encuentran muchos plugins y dependencias.
+        mavenCentral()
+        // Si el plugin de CloudStream estuviera en JitPack (o si tuvieras otros plugins allí),
+        // lo añadirías de esta manera:
+        maven { url = uri("https://jitpack.io") } // <-- ¡AÑADIDA/CORREGIDA!
+    }
+}
+
+rootProject.name = "CloudstreamPlugins" // Asegúrate de que este nombre sea correcto para tu proyecto
+
+// Este archivo establece qué proyectos están incluidos.
+// Todos los proyectos nuevos deberían incluirse automáticamente a menos que se especifiquen en la variable "disabled".
 
 val disabled = listOf<String>()
 
@@ -14,6 +30,5 @@ fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// Para incluir un solo proyecto, comenta las líneas anteriores (excepto la primera), y incluye tu plugin así:
 // include("PluginName")
