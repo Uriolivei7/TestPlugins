9963f3ad19f784f4befcac798df55f7ae44d05df
Uriolivei <1554602@senati.pe>
2025-06-21 10:56:44 -0500

    HTML enlace lacartoons

--- LacartoonsProvider/src/main/kotlin/com/example/LacartoonsProvider.kt
+++ LacartoonsProvider/src/main/kotlin/com/example/LacartoonsProvider.kt
@@ -117,6 +117,14 @@ class LacartoonsProvider:MainAPI() {
                 println("${name}: URL del iframe de Cubembed: $cubembedUrl")
                 println("${name}: Tamaño del HTML de Cubembed recibido: ${htmlContent.length} caracteres")
 
+                println("${name}: --- INICIO HTML CUBEMBED ---")
+                val chunkSize = 1000 // Tamaño de cada "pedazo" de HTML a imprimir
+                for (i in 0 until htmlContent.length step chunkSize) {
+                    val end = (i + chunkSize).coerceAtMost(htmlContent.length)
+                    println("${name}: CHUNK ${i/chunkSize}: ${htmlContent.substring(i, end)}")
+                }
+                println("${name}: --- FIN HTML CUBEMBED ---")
+
                 // Buscar la etiqueta <source> directamente en el DOM del iframe
                 val sourceElement = embedDoc.selectFirst("source[src*=.m3u8]")
 
